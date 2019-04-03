d37de3697c5e1955ee0acdc1697b9e4529422ee2
Skype <skype@Skype-Laptop-Audio.local>
2019-04-03 17:55:51 -0400

    bachelors-table ready

--- src/components/BachelorsWrapper.js
+++ src/components/BachelorsWrapper.js
@@ -9,8 +9,9 @@ class BachelorsWrapper extends Component {
           src={`https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/${this.props.contestant.img}.jpg`}
           alt={`${this.props.contestant.name}'s picture`}
         />
-        <h2> {this.props.contestant.name} </h2>
-        <h4> {this.props.contestant.age}  </h4>
+        <h6> {this.props.contestant.name} </h6>
+        <p> ({this.props.contestant.age}) {this.props.contestant.hometown} </p>
+        <p> Team: {!this.props.contestant.team? 'Not yet drafted' : this.props.contestant.team} </p> 
       </div>
     );
   }
