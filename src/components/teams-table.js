d34a8fc1c6f812b77d12989562e37ab527dcdba0
Luiz <lalmeida89@gmail.com>
2019-06-19 20:32:49 -0400

    week 3 update

--- src/components/teams-table.js
+++ src/components/teams-table.js
@@ -4,23 +4,23 @@ const TeamsTable = [
   {
     teamName: 'Melvisioner Gordon',
     teamOwner: 'Luiz',
-    bachelor1: 'Joe R.', bach1Points: 2,
-    bachelor2: 'Jed W.', bach2Points: 2,
-    totalPoints: 4
+    bachelor1: 'Joe R.', bach1Points: 3,
+    bachelor2: 'Jed W.', bach2Points: 3,
+    totalPoints: 6
   },
   {
     teamName: 'CJ Uzi Vert',
     teamOwner: 'Ibby',
     bachelor1: 'Joey J.', bach1Points: 0,
-    bachelor2: 'Mike J.', bach2Points: 2,
-    totalPoints: 2
+    bachelor2: 'Mike J.', bach2Points: 3,
+    totalPoints: 3
   },
   {
     teamName: 'CuervoSucio',
     teamOwner: 'Chris',
-    bachelor1: 'Luke P.', bach1Points: 2,
+    bachelor1: 'Luke P.', bach1Points: 3,
     bachelor2: 'Jonathan S.', bach2Points: 0,
-    totalPoints: 2
+    totalPoints: 3
   },
   {
     teamName: 'Cam Ol Toes',
@@ -33,22 +33,22 @@ const TeamsTable = [
     teamName: 'Da Behrs',
     teamOwner: 'Felix',
     bachelor1: 'Matt S.', bach1Points: 0,
-    bachelor2: 'Matt D. from FL', bach2Points: 2,
-    totalPoints: 2
+    bachelor2: 'Matt D. from FL', bach2Points: 3,
+    totalPoints: 3
   },
   {
     teamName: 'Fuck You Kareem',
     teamOwner: 'Nest',
     bachelor1: 'Kevin F.', bach1Points: 2,
-    bachelor2: 'Dustin K.', bach2Points: 2,
-    totalPoints: 4
+    bachelor2: 'Dustin K.', bach2Points: 3,
+    totalPoints: 5
   },
   {
     teamName: "Le'Veonballz",
     teamOwner: 'Jared',
-    bachelor1: 'Garret P.', bach1Points: 2,
+    bachelor1: 'Garret P.', bach1Points: 3,
     bachelor2: 'Grant E.', bach2Points: 2,
-    totalPoints: 4
+    totalPoints: 5
   },
   {
     teamName: 'DeVonta get 2 know ya',
@@ -61,29 +61,29 @@ const TeamsTable = [
     teamName: 'DBreezyMVPeezy',
     teamOwner: 'Liam',
     bachelor1: 'Tyler G.', bach1Points: 0,
-    bachelor2: 'Tyler C.', bach2Points: 2,
-    totalPoints: 2
+    bachelor2: 'Tyler C.', bach2Points: 3,
+    totalPoints: 3
   },
   {
     teamName: "It's A Fire!",
     teamOwner: 'Jimmy',
-    bachelor1: 'Peter W,', bach1Points: 2,
-    bachelor2: 'Dylan B.', bach2Points: 2,
-    totalPoints: 4
+    bachelor1: 'Peter W,', bach1Points: 3,
+    bachelor2: 'Dylan B.', bach2Points: 3,
+    totalPoints: 6
   },
   {
     teamName: 'Old Men Drinkin Beer',
     teamOwner: 'Ski',
-    bachelor1: 'Connor S.', bach1Points: 2,
+    bachelor1: 'Connor S.', bach1Points: 3,
     bachelor2: 'Luke S.', bach2Points: 1,
-    totalPoints: 3
+    totalPoints: 4
   },
   {
     teamName: 'Oh Saquon You See',
     teamOwner: 'Alan',
     bachelor1: 'Jon Paul James', bach1Points: 1,
-    bachelor2: 'Ben G.', bach2Points: 2,
-    totalPoints: 3
+    bachelor2: 'Ben G.', bach2Points: 3,
+    totalPoints: 4
   }
 ]
 
